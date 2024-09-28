dfb271e7f3e2ede5689032d63949bb0490ddeaa6
Franco-JG <jeftefranco@outlook.com>
2024-09-28 16:31:21 -0600

    first commit

---
+++ src/core/renderer.ts
@@ -0,0 +1,22 @@
+import { WebGLRenderer, PCFShadowMap } from "three";
+import { sizes } from "./camera.ts";
+
+const canvas: HTMLElement = document.querySelector('#scene') as HTMLElement
+
+export const renderer =  new WebGLRenderer({
+    canvas,
+  })
+  
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = PCFShadowMap
+
+export function updateRenderer() {
+	renderer.setSize(sizes.width, sizes.height)
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+}
+
+window.addEventListener('resize', () => {
+	sizes.width = window.innerWidth
+	sizes.height =  window.innerHeight
+	updateRenderer()
+})
---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import './style.css' 
+
+import * as THREE from 'three'
+import './core/orbit-controls.ts'
+import { camera } from './core/camera.ts';
+import { renderer, updateRenderer } from './core/renderer.ts'
+import { ambientLight, directionalLight } from './core/lights.ts';
+
+//Scene
+const scene = new THREE.Scene()
+scene.add(ambientLight)
+scene.add(directionalLight)
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(1, 32, 32),
+  new THREE.MeshToonMaterial({
+    color: new THREE.Color('#e04358'),
+  }),
+);
+sphere.position.set(0,2,0)
+sphere.castShadow = true
+
+scene.add(sphere)
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(10,10,10,10),
+  new THREE.MeshToonMaterial({
+    color: '#444',
+    side: THREE.DoubleSide
+  })
+);
+plane.rotateX(Math.PI/180 * 90)
+plane.receiveShadow = true
+
+scene.add(plane);
+scene.add(camera);
+updateRenderer();
+
+const animate = () => {
+  renderer.render(scene, camera)
+  requestAnimationFrame(animate)
+}
+
+animate()
